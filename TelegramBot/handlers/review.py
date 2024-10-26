453ddbddc5cf81e50aee11e974998edfbca4359e
abanov20 <asdfgabanov@gmail.com>
2024-10-27 00:04:09 +0600

    Add files via upload

--- TelegramBot/handlers/review.py
+++ TelegramBot/handlers/review.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F, types
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-import sqlite3
+
 
 from bot_config import database
 
@@ -17,6 +17,14 @@ class PizzaReview(StatesGroup):
 
 @review_router.callback_query(F.data == "review")
 async def start_review_handler(callback: types.CallbackQuery, state: FSMContext):
+    user_tg_id = database.fetch(
+        query="""SELECT * FROM review WHERE tg_id = ?""",
+        params=(callback.from_user.id,)
+    )
+    print(user_tg_id)
+    if len(user_tg_id) > 0:
+        await callback.message.answer("Нельзя проходить опрос повторно!")
+        return
     await state.set_state(PizzaReview.name)
     await callback.message.answer("Как Вас зовут?")
 
@@ -27,13 +35,13 @@ async def process_name(message: types.Message, state: FSMContext):
     await message.answer("Ваш номер телефона?")
 
 @review_router.message(PizzaReview.phone_number)
-async def procces_phone_number(message: types.Message, state: FSMContext):
+async def process_phone_number(message: types.Message, state: FSMContext):
     await state.update_data(phone_number=message.text)
     await state.set_state(PizzaReview.visit_date)
     await message.answer("Дата вашего посещения нашего заведения")
 
 @review_router.message(PizzaReview.visit_date)
-async def procces_food_rating(message: types.Message, state: FSMContext):
+async def process_food_rating(message: types.Message, state: FSMContext):
     await state.update_data(visit_date=message.text)
     await state.set_state(PizzaReview.food_rating)
     kb = types.ReplyKeyboardMarkup(
@@ -51,38 +59,39 @@ async def procces_food_rating(message: types.Message, state: FSMContext):
     await message.answer("Качество еды", reply_markup=kb)
 
 @review_router.message(PizzaReview.food_rating)
-async def proccesing_cleanliness_rating(message: types.Message, state: FSMContext):
+async def process_cleanliness_rating(message: types.Message, state: FSMContext):
     await state.update_data(food_rating=message.text)
     await state.set_state(PizzaReview.cleanliness_rating)
     ckb = types.ReplyKeyboardMarkup(
         keyboard=[
             [
-            types.KeyboardButton(text="1"),
-            types.KeyboardButton(text="2"),
-            types.KeyboardButton(text="3"),
-            types.KeyboardButton(text="4"),
-            types.KeyboardButton(text="5")
+                types.KeyboardButton(text="1"),
+                types.KeyboardButton(text="2"),
+                types.KeyboardButton(text="3"),
+                types.KeyboardButton(text="4"),
+                types.KeyboardButton(text="5")
             ]
         ],
         resize_keyboard=True
     )
     await message.answer("Чистота заведения", reply_markup=ckb)
 
-    @review_router.message(PizzaReview.cleanliness_rating)
-    async def process_extra_comments(message: types.Message, state: FSMContext):
-        await state.update_data(cleanliness_rating=message.text)
-        await state.set_state(PizzaReview.extra_comments)
-        await message.answer("Дополнительные комментарии или жалоба?")
+@review_router.message(PizzaReview.cleanliness_rating)
+async def process_extra_comments(message: types.Message, state: FSMContext):
+    await state.update_data(cleanliness_rating=message.text)
+    await state.set_state(PizzaReview.extra_comments)
+    await message.answer("Дополнительные комментарии или жалоба?")
 
 @review_router.message(PizzaReview.extra_comments)
-async def process_comment(message: types.Message, state: FSMContext):
+async def process_comments(message: types.Message, state: FSMContext):
     await state.update_data(extra_comments=message.text)
     data = await state.get_data()
+    tg_id = message.from_user.id
 
     database.execute(
         query="""
-            INSERT INTO review (name, phone_number, visit_date, food_rating, cleanliness_rating, comment)
-            VALUES (?, ?, ?, ?, ?, ?)
+            INSERT INTO review (name, phone_number, visit_date, food_rating, cleanliness_rating, tg_id, comment)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
             """,
         params=(
             data['name'],
@@ -90,10 +99,11 @@ async def process_comment(message: types.Message, state: FSMContext):
             data['visit_date'],
             data['food_rating'],
             data['cleanliness_rating'],
-            message.text
+            tg_id,
+            data['extra_comments']
         )
     )
 
     await message.answer(
-        f"Спасибо за ваш отзыв, {data['name']}!\nВаш комментарий: {message.text}")
+        f"Спасибо за ваш отзыв, {data['name']}!\nВаш комментарий: {data['extra_comments']}")
     await state.clear()
